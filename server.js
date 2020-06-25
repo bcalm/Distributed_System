5c101c74937a59dd06473c9ac8fef6b788eeb886
Vikram Singh <58028680+bcalm@users.noreply.github.com>
2020-06-25 20:17:35 +0530

    Added worker.js. Whoever hit the server will get a uniq id

--- server.js
+++ server.js
@@ -1,7 +1,26 @@
 const express = require('express');
+const http = require('http');
 const app = express();
-const {processImage} = require('./processImage');
 const port = process.env.port || 8080;
+
+let id = 0;
+
+const getWorkerOptions = () => {
+  return {
+    host: 'localhost',
+    port: '5000',
+    method: 'POST',
+  };
+};
+
+const delegateToWorker = function (id, {name, count, height, width, tags}) {
+  const options = getWorkerOptions();
+  options.path = `/process/${id}/${count}/${height}/${width}/${tags}`;
+  const req = http.request(options, (res, err) => {
+    console.log('Got from worker', res.statusCode);
+  });
+  req.end();
+};
 
 app.use((req, res, next) => {
   console.log(`${req.url} ${req.method}`);
@@ -9,15 +28,10 @@ app.use((req, res, next) => {
 });
 
 app.post('/process/:name/:count/:height/:width/:tags', (req, res) => {
-  processImage(req.params)
-    .then((tags) => {
-      console.log(tags);
-      return tags;
-    })
-    .then((tags) => {
-      res.write(JSON.stringify(tags));
-      res.end();
-    });
+  delegateToWorker(id, req.params);
+  res.send(`${id}`);
+  res.end();
+  id++;
 });
 
 app.listen(port, () => console.log(`app is listening on port ${port}`));
