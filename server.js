ed9d0c06c3547919feb92587ba092363b55a4a92
Vikram Singh <58028680+bcalm@users.noreply.github.com>
2020-06-27 14:38:27 +0530

    Added worker server relationship.

--- server.js
+++ server.js
@@ -3,7 +3,16 @@ const http = require('http');
 const app = express();
 const port = process.env.port || 8080;
 
+let isWorkerFree = true;
 let id = 0;
+const jobs = [];
+
+setInterval(() => {
+  if (isWorkerFree && jobs.length > 0) {
+    const job = jobs.shift();
+    delegateToWorker(id, job);
+  }
+}, 1000);
 
 const getWorkerOptions = () => {
   return {
@@ -13,22 +22,29 @@ const getWorkerOptions = () => {
   };
 };
 
+app.use((req, res, next) => {
+  console.log(`${req.url} ${req.method}`);
+  next();
+});
+
+app.post('/process/completedJob/:id/:tags', (req, res) => {
+  console.log(req.params);
+  isWorkerFree = true;
+  res.end();
+});
+
 const delegateToWorker = function (id, {name, count, height, width, tags}) {
   const options = getWorkerOptions();
   options.path = `/process/${id}/${count}/${height}/${width}/${tags}`;
   const req = http.request(options, (res, err) => {
     console.log('Got from worker', res.statusCode);
   });
+  isWorkerFree = false;
   req.end();
 };
 
-app.use((req, res, next) => {
-  console.log(`${req.url} ${req.method}`);
-  next();
-});
-
 app.post('/process/:name/:count/:height/:width/:tags', (req, res) => {
-  delegateToWorker(id, req.params);
+  jobs.push(req.params);
   res.send(`${id}`);
   res.end();
   id++;
--- worker.js
+++ worker.js
@@ -1,12 +1,30 @@
 const express = require('express');
+const http = require('http');
 const app = express();
 const {processImage} = require('./processImage');
 const port = 5000;
+
+const getServerOptions = () => {
+  return {
+    host: 'localhost',
+    port: '8080',
+    method: 'POST',
+  };
+};
 
 app.use((req, res, next) => {
   console.log(`${req.url} ${req.method}`);
   next();
 });
+
+const informWorkerFree = function (id, tags) {
+  const options = getServerOptions();
+  options.path = `/process/completedJob/${id}/${tags}`;
+  const req = http.request(options, (res, err) => {
+    console.log('Got from server', res.statusCode);
+  });
+  req.end();
+};
 
 app.post('/process/:id/:count/:height/:width/:tags', (req, res) => {
   processImage(req.params)
@@ -15,6 +33,7 @@ app.post('/process/:id/:count/:height/:width/:tags', (req, res) => {
       return tags;
     })
     .then((tags) => {
+      informWorkerFree(req.params.id, tags);
       res.write(JSON.stringify(tags));
       res.end();
     });
